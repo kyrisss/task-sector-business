e82dd981180364716cc2062b0369869a79628bbb
kyrisss <kornev.roma62@gmail.com>
2022-05-04 22:46:21 +0300

    add searchPanel

---
+++ src/components/SearchPanel/SearchPanel.tsx
@@ -0,0 +1,25 @@
+import { connect } from 'react-redux'
+import { SET_SEARCH } from '../redux/postsReducer'
+import search from './search.svg'
+import './style.scss'
+
+interface PropsType{
+
+}
+
+const SearchPanel: React.FC<PropsType> = (props) => {
+
+
+    const onChangeInput = (e:React.ChangeEvent<HTMLInputElement>) =>{
+        SET_SEARCH(e.target.value)
+    }
+
+    return(
+        <div className="search">
+            <input className="search__input" type="text" placeholder="Поиск" onChange={onChangeInput}/>
+            <img className="search__icon" src={search} alt="search" />
+        </div>
+    )
+}
+
+export default connect(null,{SET_SEARCH})(SearchPanel)
